6e2d1da90b6236da198bd7ffb1a93102e5cfb595
Blue <blueloveshisfriends@gmail.com>
2020-09-06 20:08:38 +0300

    Begin ParticsCommand, add Mask, add Partics.Particle

--- src/main/java/io/github/bluelhf/partics/Partics.java
+++ src/main/java/io/github/bluelhf/partics/Partics.java
@@ -1,6 +1,7 @@
 package io.github.bluelhf.partics;
 
 import io.github.bluelhf.partics.util.ParticleUtil;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Partics extends JavaPlugin {
@@ -19,7 +20,40 @@ public final class Partics extends JavaPlugin {
         return particleUtil;
     }
 
-    public Partics get() {
+    public static Partics get() {
         return (Partics) JavaPlugin.getProvidingPlugin(Partics.class);
+    }
+
+    static class Particle {
+        private Location location;
+        private Particle particle;
+        private int period;
+
+        public Particle(Location location, Particle particle, int period) {
+            this.location = location;
+            this.particle = particle;
+            this.period = period;
+        }
+
+        /**
+         * @return The period between particle spawns, in ticks (1/20ths of a second).
+         * */
+        public int getPeriod() {
+            return period;
+        }
+
+        /**
+         * @return The location of this particle.
+         * */
+        public Location getLocation() {
+            return location;
+        }
+
+        /**
+         * @return The {@link org.bukkit.Particle} this particle creates.
+         * */
+        public Particle getParticle() {
+            return particle;
+        }
     }
 }
--- src/main/java/io/github/bluelhf/partics/command/ParticsCommand.java
+++ src/main/java/io/github/bluelhf/partics/command/ParticsCommand.java
@@ -1,14 +1,100 @@
 package io.github.bluelhf.partics.command;
 
+import com.moderocky.mask.command.*;
+import com.moderocky.mask.template.WrappedCommand;
+import io.github.bluelhf.partics.Partics;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.Particle;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ParticsCommand implements CommandExecutor {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+public class ParticsCommand extends Commander<CommandSender> implements WrappedCommand  {
+    @Override
+    protected CommandImpl create() {
+        return command("partics")
+            .arg("create", arg((sender, input) -> {
+                    String typeStr = (String) input[0];
+                    Optional<Particle> maybeParticle = Partics.get().getPUtil().fromMCN(typeStr);
+
+                    if (maybeParticle.isEmpty()) {
+                        sender.spigot().sendMessage(
+                            new ComponentBuilder()
+                            .append("'").append(typeStr).append("'")
+                            .append(" is not a valid particle!").color(ChatColor.RED)
+                            .create()
+                        );
+                        return;
+                    }
+
+
+                    int count     = input.length >= 2 ? (int) input[1] : 1;
+
+                    double speed  = input.length >= 3 ? (double) input[2] : 0;
+                    double dX     = input.length >= 4 ? (double) input[3] : 0;
+                    double dY     = input.length >= 5 ? (double) input[4] : 0;
+                    double dZ     = input.length >= 6 ? (double) input[5] : 0;
+
+                    boolean force = input.length >= 7 && (boolean) input[6];
+
+
+
+                },
+                new ArgString().setLabel("type").setRequired(true),
+                new ArgInteger().setLabel("count").setRequired(false),
+                new ArgNumber().setLabel("speed").setRequired(false),
+                new ArgNumber().setLabel("delta_x").setRequired(false),
+                new ArgNumber().setLabel("delta_y").setRequired(false),
+                new ArgNumber().setLabel("delta_z").setRequired(false),
+                new ArgBoolean().setLabel("force").setRequired(false)
+                )
+            );
+    }
+
+    @Override
+    public CommandSingleAction<CommandSender> getDefault() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getCommand() {
+        return "partics";
+    }
+
+    @Override
+    public @NotNull List<String> getAliases() {
+        return Arrays.asList("pcs", "partic");
+    }
+
+    @Override
+    public @NotNull String getUsage() {
+        return "No usage defined.";
+    }
+
+    @Override
+    public @NotNull String getDescription() {
+        return "Main command for Partics.";
+    }
+
+    @Override
+    public @Nullable String getPermission() {
+        return "partics.use";
+    }
+
+    @Override
+    public @Nullable String getPermissionMessage() {
+        return "§cNo permission!";
+    }
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
-
-        return true;
+        return false;
     }
 }
